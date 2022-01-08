3f0a9b2d1e434c5d2774b62a3c963051611b4a64
Tresor1223 <lysekonan3@gmail.com>
2022-01-08 16:55:41 +0000

    Test

---
+++ components/test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { API_KEY } from '../constants';
+
+export default function Test(){
+    return (
+        <>
+            <h3>Application de météo</h3>
+            <p>API KEY:{API_KEY}</p>
+        </>
+    );
+}
---
+++ constants.js
@@ -0,0 +1,2 @@
+//les constantes
+export const API_KEY = 'ac8944814fd4019bee7b37d9ca2f94f0';
